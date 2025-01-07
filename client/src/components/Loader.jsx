590aa15e43647eab50d971c5f5e7d09a0c62887d
MrVimalKumar <vimal.cst.2021@snsce.ac.in>
2025-01-07 18:54:00 +0530

    Ready

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import"../styles/Loader.scss"
+
+const Loader = () => {
+  return (
+    <div Classname='loader'>
+        <div className='loader-inner'></div>
+    </div>
+  )
+}
+
+export default Loader
